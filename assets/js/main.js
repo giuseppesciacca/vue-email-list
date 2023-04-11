e52aea261725f0bf2af902dfae01cfb85b294e1e
giuseppesciacca <sciaccagi@gmail.com>
2023-04-11 16:06:21 +0200

    [add] print list of email on display

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,7 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            email: null,
+            numEmail: 10,
+            listEmail: [],
+        }
+    },
+    mounted() {
+        for (let i = 0; i < this.numEmail; i++) {
+            axios
+                .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                .then(response => {
+                    this.email = response.data.response;
+                    console.log(this.email);
+                    this.listEmail.push(this.email);
+                })
         }
     }
 }).mount('#app')
